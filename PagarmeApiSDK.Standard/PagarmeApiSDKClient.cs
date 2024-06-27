33fb89002913e20eae33b9c06e9cc0225f3ce777
André Luis <andre.lsilva@pagar.me>
2024-06-27 09:59:38 -0300

    adjust version

--- PagarmeApiSDK.Standard/PagarmeApiSDKClient.cs
+++ PagarmeApiSDK.Standard/PagarmeApiSDKClient.cs
@@ -33,7 +33,7 @@ namespace PagarmeApiSDK.Standard
         };
 
         private readonly GlobalConfiguration globalConfiguration;
-        private const string userAgent = "PagarmeApiSDK - DotNet 6.8.9";
+        private const string userAgent = "PagarmeApiSDK - DotNet 6.8.10";
         private readonly Lazy<ISubscriptionsController> subscriptions;
         private readonly Lazy<IOrdersController> orders;
         private readonly Lazy<IPlansController> plans;
